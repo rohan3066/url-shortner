e8d4b27779fd784f3970adda2d5a89ce617115b8
rohan3066 <rohanchavan3066@gmail.com>
2025-06-08 17:44:46 +0530

    redeploy

--- app.js
+++ app.js
@@ -101,6 +101,6 @@ const server = createServer(async (req, res) => {
   }
 });
 
-server.listen(8080, () => {
-  console.log("server is running at 8080");
+server.listen(4040, () => {
+  console.log("server is running at 4040");
 });
